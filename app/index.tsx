84b22fd3e7e337f1b24981fed21fcf4453dc01a9
ngocduy <ngocduy250604@gmail.com>
2025-05-12 20:31:44 +0700

    new commit

--- app/index.tsx
+++ app/index.tsx
@@ -1,5 +1,6 @@
 import { Text, View } from "react-native";
 
+var name = "Trương Ngọc Duy"
 export default function Index() {
   return (
     <View
@@ -9,7 +10,7 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>Hello my name is {name}!</Text>
     </View>
   );
 }
